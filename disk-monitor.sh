6b7cfb3e722d557bcb9f8e934bc829f973b89ac5
Siva <siva.subramanian3@icloud.com>
2026-02-03 19:45:17 +0530

    This script monitors disk usage and sends an alert if usage exceeds a specified threshold.

---
+++ disk-monitor.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+# This script monitors disk usage and sends an alert if usage exceeds a specified threshold.
+
+THRESHOLD=80
+EMAIL="abc@company.com"
+LOGFILE="/var/log/disk-monitor.log"
+
+df -hP -x tmpfs =x devtmpfs -x overlay -x squashfs | \
+awk 'NR>1 {print $5 " " $6}' | \
+while read usage mount
+do
+     usage=${usage%\%}
+        if [ "$usage" -ge "$THRESHOLD"]; then
+            MESSAGE ="ALERT: Disk usage on $mount is at $usage% on $hostname as of $(date)"
+            echo $MESSAGE >> $LOGFILE
+            echo $MESSAGE | mail -s "Disk Usage Alert on $hostname" $EMAIL
+        fi
+done
